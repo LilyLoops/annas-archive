0e5395fab63b8f06624cb8ff2f1d5d82a419c75d
AnnaArchivist <mailto:1-AnnaArchivist@users.noreply.annas-software.org>
2024-09-30 00:00:00 +0000

    zzz

--- data-imports/scripts/download_libgenli.sh
+++ data-imports/scripts/download_libgenli.sh
@@ -17,22 +17,22 @@ cd /temp-dir/libgenli_db
 
 # rclone -vP --include 'libgen_new.*' --max-depth 1 --check-first --checkers 1 --transfers 1 --size-only copy --retries=100 --low-level-retries=1000 --http-url="https://libgen.li/dbdumps/" :http: /temp-dir/libgenli_db/
 
-# for i in $(seq -w 1 5); do # retries
-#     rclone copy :ftp:/upload/db/ /temp-dir/libgenli_db/ --ftp-host=ftp.libgen.lc --ftp-user=anonymous --ftp-pass=$(rclone obscure dummy) --size-only --progress --multi-thread-streams=1 --transfers=1
-# done
-
-curl --fail -L -O "https://libgen.li/dbdumps/libgen_new.zip" || curl --fail -L -O "https://libgen.gs/dbdumps/libgen_new.zip" || curl --fail -L -O "https://libgen.vg/dbdumps/libgen_new.zip" || curl --fail -L -O "https://libgen.pm/dbdumps/libgen_new.zip"
-for i in $(seq -w 1 64); do
-    # Using curl here since it only accepts one connection from any IP anyway,
-    # and this way we stay consistent with `libgenli_proxies_template.sh`.
-
-    # Server doesn't support resuming??
-    # curl -L -C - -O "https://libgen.li/dbdumps/libgen_new.part0${i}.rar"
-    
-    # Try bewteen these:
-    # *.li, *.gs, *.vg, *.pm
-    # curl --fail -L -O "https://libgen.lc/dbdumps/libgen_new.part0${i}.rar" || curl --fail -L -O "https://libgen.li/dbdumps/libgen_new.part0${i}.rar" || curl --fail -L -O "https://libgen.gs/dbdumps/libgen_new.part0${i}.rar" || curl --fail -L -O "https://libgen.vg/dbdumps/libgen_new.part0${i}.rar" || curl --fail -L -O "https://libgen.pm/dbdumps/libgen_new.part0${i}.rar"
-    curl --fail -L -O "https://libgen.li/dbdumps/libgen_new.z${i}" || curl --fail -L -O "https://libgen.gs/dbdumps/libgen_new.z${i}" || curl --fail -L -O "https://libgen.vg/dbdumps/libgen_new.z${i}" || curl --fail -L -O "https://libgen.pm/dbdumps/libgen_new.z${i}"
+for i in $(seq -w 1 5); do # retries
+    rclone copy :ftp:/upload/dbbackup/ /temp-dir/libgenli_db/ --ftp-host=ftp.libgen.lc --ftp-user=anonymous --ftp-pass=$(rclone obscure dummy) --size-only --progress --multi-thread-streams=1 --transfers=1
 done
+
+# curl --fail -L -O "https://libgen.li/dbdumps/libgen_new.zip" || curl --fail -L -O "https://libgen.gs/dbdumps/libgen_new.zip" || curl --fail -L -O "https://libgen.vg/dbdumps/libgen_new.zip" || curl --fail -L -O "https://libgen.pm/dbdumps/libgen_new.zip"
+# for i in $(seq -w 1 64); do
+#     # Using curl here since it only accepts one connection from any IP anyway,
+#     # and this way we stay consistent with `libgenli_proxies_template.sh`.
+
+#     # Server doesn't support resuming??
+#     # curl -L -C - -O "https://libgen.li/dbdumps/libgen_new.part0${i}.rar"
+    
+#     # Try bewteen these:
+#     # *.li, *.gs, *.vg, *.pm
+#     # curl --fail -L -O "https://libgen.lc/dbdumps/libgen_new.part0${i}.rar" || curl --fail -L -O "https://libgen.li/dbdumps/libgen_new.part0${i}.rar" || curl --fail -L -O "https://libgen.gs/dbdumps/libgen_new.part0${i}.rar" || curl --fail -L -O "https://libgen.vg/dbdumps/libgen_new.part0${i}.rar" || curl --fail -L -O "https://libgen.pm/dbdumps/libgen_new.part0${i}.rar"
+#     curl --fail -L -O "https://libgen.li/dbdumps/libgen_new.z${i}" || curl --fail -L -O "https://libgen.gs/dbdumps/libgen_new.z${i}" || curl --fail -L -O "https://libgen.vg/dbdumps/libgen_new.z${i}" || curl --fail -L -O "https://libgen.pm/dbdumps/libgen_new.z${i}"
+# done
 
 #for i in $(seq -w 6 47); do curl --fail -L -O "https://libgen.lc/dbdumps/libgen_new.part0${i}.rar" || curl --fail -L -O "https://libgen.li/dbdumps/libgen_new.part0${i}.rar" || curl --fail -L -O "https://libgen.gs/dbdumps/libgen_new.part0${i}.rar" || curl --fail -L -O "https://libgen.vg/dbdumps/libgen_new.part0${i}.rar" || curl --fail -L -O "https://libgen.pm/dbdumps/libgen_new.part0${i}.rar"; done
